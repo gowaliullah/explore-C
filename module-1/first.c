ceefa10336393b3a8ddbbe353aa50babf88d4662
MD Waliullah <waliullah.godev@gmail.com>
2025-10-05 07:42:43 +0600

    data_types

--- module-1/first.c
+++ module-1/first.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+// int - 4 bytes 
+// float - 4 bytes 
+// char - 1 byte 
+ 
+
+int data_types(){
+    int num1 = 1000;
+    float f = 10.9;
+    char ch = "@";
+    char num_cha = "11, number is possible to declare as a char";
+    return 0;
+}
+
 int main()
 {
-    printf("hello world");
+    printf("hello world \t --> \n");
+    printf("from new Era");
     return 0;
 }
