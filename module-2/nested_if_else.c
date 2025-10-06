e7e3c2dee0547481e24c044f5d68a81e4c5e21d2
MD Waliullah <waliullah.godev@gmail.com>
2025-10-06 14:43:33 +0600

    nested if else

--- module-2/nested_if_else.c
+++ module-2/nested_if_else.c
@@ -4,11 +4,15 @@ int main() {
     int a = 10;
     int b = 3;
 
-    // If-Else Example
+    // Nested If-Else Example
     if (a > b) {
         printf("A is greater than B\n");
+        if (a - b > 5) {
+            printf("The difference is greater than 5\n");
+        } else {
+            printf("The difference is not greater than 5\n");
+        }
     } else if (a == b) {
-        // code
         printf("A is equal to B\n");
     } else {
         printf("A is not greater than B\n");
