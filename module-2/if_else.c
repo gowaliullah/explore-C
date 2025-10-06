f283f90ec79fd076d9e421e57ced30c0f43d01ad
MD Waliullah <waliullah.godev@gmail.com>
2025-10-06 14:37:58 +0600

    If else

---
+++ module-2/if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    // If-Else Example
+    if (a > b) {
+        printf("A is greater than B\n");
+    } else {
+        printf("A is not greater than B\n");
+    }
+
+    return 0;
+}
