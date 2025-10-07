bb410265014628be29a9c6b48e9089b9e9e72234
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 21:30:14 +0600

    continue

--- module-3/loop-with-condition.c
+++ module-3/loop-with-condition.c
@@ -4,8 +4,10 @@ int main()
 {
     for (int i = 0; i < 30; i++) {
         if (i % 2 == 0) {
-            printf("%d\n", i);
-        } 
+            printf("%d is even\n", i);
+        } else {
+            printf("%d is odd\n", i);
+        }
     }
     return 0;
 }
