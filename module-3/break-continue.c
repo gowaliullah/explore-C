bdc928e7211e3a6210b0643be89a1816954ee576
MD Waliullah <waliullah.godev@gmail.com>
2025-10-07 21:33:15 +0600

    break statement

--- module-3/break-continue.c
+++ module-3/break-continue.c
@@ -3,11 +3,22 @@
 
 int main()
 {
+
+    // continue statement
     for (int i = 0; i < 30; i++) {
         if ( (i % 5) == 0) {
             continue;
         }
-        printf("%d\n", i);  
+        printf("%d\n", i);
     }
+
+    // break statement
+    for (int i = 0; i < 30; i++) {
+        if ( (i / 5) == 3) {
+            break;
+        }
+        printf("%d\n", i);
+    }
+
     return 0;
 }
