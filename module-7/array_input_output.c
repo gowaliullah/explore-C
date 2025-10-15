db15815047dcf33d22ed68f23880a3f749b7fdc5
MD Waliullah <waliullah.godev@gmail.com>
2025-10-15 19:48:48 +0600

    array input and output

---
+++ module-7/array_input_output.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", i);
+    }
+
+    return 0;
+}
