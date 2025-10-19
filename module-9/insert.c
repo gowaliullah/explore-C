dca492fe627905435cc9b86aba71918ce93846fa
MD Waliullah <waliullah.godev@gmail.com>
2025-10-19 10:55:20 +0600

    insert

--- module-9/insert.c
+++ module-9/insert.c
@@ -6,7 +6,7 @@ int main()
     int n;
     scanf("%d", &n);
 
-    int a[n];
+    int a[n + 1];
 
     for (int i = 0; i < n; i++)
     {
@@ -16,6 +16,17 @@ int main()
     int inx, val;
     scanf("%d %d", &inx, &val);
 
+    for (int i = n; i >= inx + 1; i--)
+    {
+        a[i] = a[i - 1];
+    }
+
+    a[inx] = val;
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", a[i]);
+    }
 
     return 0;
 }
