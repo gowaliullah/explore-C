1b351dc8ea270b6bb5eb21d505ded109f9b54e53
MD Waliullah <waliullah.godev@gmail.com>
2025-10-20 09:57:01 +0600

    initialize

--- module-10/string_inout_with_spacec.c
+++ module-10/string_inout_with_spacec.c
@@ -2,20 +2,18 @@
 
 int main()
 {
-
      // at least 1 ghor base nite hobe
-
     char s[50];
-
     gets(s); 
-
     fgets(s, 20, stdin); // count null and space
 
-
-
-
     scanf("%d", s[5]);
-     
+
+
+
+    // with initialize
+    char s2[6] = {"H", "E", "L", "L", "O", "\n"};
+    printf("%d", s2);     
 
     return 0;
 }
