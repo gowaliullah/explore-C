66719b99ba59705d908be0fea3e1326944b1109a
MD Waliullah <waliullah.godev@gmail.com>
2025-10-20 10:22:21 +0600

    build in func

--- module-10/length_of_a_string.c
+++ module-10/length_of_a_string.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     
     
     char s[101];
-
     scanf("%s", s);
 
-
     int cnt = 0;
-
     for (int i = 0; s[i] != '\0'; i++)
     {
        cnt++;
     }
-    
- printf("%d", cnt);
+
+    printf("%d", cnt);
+
+    // build in func
+
+    int sz = strlen(s);
+    printf("%d", sz);
 
     return 0;
 }
