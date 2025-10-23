99bb07b461861a86706c27aafb0d3c45474f7c4d
MD Waliullah <waliullah.godev@gmail.com>
2025-10-23 18:41:53 +0600

    string concate with buildin func

--- module-11/sstring_concate.c
+++ module-11/sstring_concate.c
@@ -6,6 +6,10 @@ int main()
     char a[101], b[101];
     scanf("%s %s", &a, &b);
 
+    // with build-in 
+    strcat(a, b);
+
+        // manually copy 
     for (int i = 0; i <= strlen(b); i++)
     {
        a[strlen(a) + i] = b[i];
