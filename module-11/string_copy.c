c5515c1488ed7c3f02ef4166f7e265f77c965885
MD Waliullah <waliullah.godev@gmail.com>
2025-10-23 16:53:50 +0600

    string copy with build-in func

--- module-11/string_copy.c
+++ module-11/string_copy.c
@@ -7,6 +7,10 @@ int main()
     char a[101], b[101];
     scanf("%s %s", &a, &b);
 
+    // with build-in 
+    strcpy(a, b);
+
+    // manually copy 
     for (int i = 0; i <= strlen(b); i++)
     {
        a[i] = b[i];
