dfd61bad65b5584da99f9c55606fc0069cc89c20
MD Waliullah <waliullah.godev@gmail.com>
2025-11-03 14:27:22 +0600

    sum of two values equal

---
+++ module-13/sum_of_2_values_equal.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    int x;
+    scanf("%d", &x);
+
+    int flag = 0; 
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if( a[i] + a[j] == x ) {   
+               flag = 1;
+                break;
+            }
+        }
+        
+    }
+
+    if (flag)
+    {
+         printf("YES");
+    } else {
+         printf("NO");
+    }
+
+    return 0;
+}
