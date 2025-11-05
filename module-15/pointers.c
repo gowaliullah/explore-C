f1317048e82763b8b4319248819417763db394cf
MD Waliullah <waliullah.godev@gmail.com>
2025-11-05 16:20:39 +0600

    refactor pointer example and improve output formatting

--- module-15/pointers.c
+++ module-15/pointers.c
@@ -3,14 +3,21 @@
 int main()
 {
     
+
+    // val --> add = ref
+    // add --> val = deref
+
     int x = 10;
     printf("%d\n", x);
+    printf("%p\n", &x);
+
     
     int* ptr;
-    
     ptr = &x;
-    printf("%d\n", ptr);
-    printf("%d\n", &ptr);
+    *ptr = 99;
+
+
+    printf("%d\n", *ptr);
 
     return 0;
 }
